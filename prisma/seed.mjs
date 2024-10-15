55d710036ed03e16bc07c50a7b0fe7c74af28ac9
athanathanthan <jdtrres@gmail.com>
2024-10-15 10:13:12 +0800

    [USER] Added : User Seeder : #29

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -1,22 +1,51 @@
-import { PrismaClient } from './generated/client/index.js';
-
-const prisma = new PrismaClient()
+import { PrismaClient } from "./generated/client/index.js";
+import bcrypt from "bcrypt";
+const prisma = new PrismaClient();
 
 async function main() {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const users = await prisma.users.upsert({
-        where: { email: 'rico.guinana@fujitsu.com' },
-        update: {},
-        create: {
-            email: 'rico.guinana@fujitsu.com',
-            name: 'Rico Guinanao',
-        },
-    })
+  const users = [
+    {
+      name: "Gian Maaghop",
+      email: "gian.maaghop@fujitsu.com",
+      password: "testpassword123456789",
+    },
+    {
+      name: "Mico Montanez",
+      email: "mico.montanez@fujitsu.com",
+      password: "testpassword123456789",
+    },
+    {
+      name: "Jonathan de Torres",
+      email: "jdtrres@gmail.com",
+      password: "testpassword123456789",
+    },
+    {
+      name: "Rico Guinanao",
+      email: "rico.guinana@fujitsu.com",
+      password: "testpassword123456789",
+    },
+  ];
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  for (const user of users) {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    await prisma.users.upsert({
+      where: { email: user.email },
+      update: {},
+      create: {
+        name: user.name,
+        email: user.email,
+        password: hashedPassword,
+      },
+    });
+    console.log(`User ${user.name} upserted successfully.`);
+  }
+
+  console.log("Seeding completed successfully.");
 }
 
 main()
-    .then(() => prisma.$disconnect)
-    .catch( async (error) => {
-        console.error(error)
-        await prisma.$disconnect()
-    })
+  .then(() => prisma.$disconnect)
+  .catch(async (error) => {
+    console.error(error);
+    await prisma.$disconnect();
+  });
